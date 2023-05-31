deb0b5cd7346d8ac4085d19cc36a827d917466af
Barry25000 <barryengler@gmail.com>
2023-05-31 12:14:49 -0500

    save

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,31 @@
 const currentElement = document.getElementById("weatherinfo");
 
+// fetch geo coordinates
+
 fetch(
-  "https://api.openweathermap.org/data/2.5/weather?q=chicago&APPID=f143fe1fd933ca340292950f394916e2&units=imperial"
+  "http://api.openweathermap.org/geo/1.0/direct?q=chicago,IL,&limit=1&appid=f143fe1fd933ca340292950f394916e2"
+)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (lat_lon) {
+    // console.log(lat_lon);
+    const lat = lat_lon[0].lat;
+    const lon = lat_lon[0].lot;
+    const state = lat_lon[0].state;
+  });
+
+// fetch current weather condition , hard coded at the moment
+fetch(
+  ("https://api.openweathermap.org/data/2.5/weather?q=" + lat) &
+    (lon + "&APPID=f143fe1fd933ca340292950f394916e2&units=imperial")
 )
   .then(function (response) {
     return response.json();
   })
   .then(function (response) {
     const current = response;
-    console.log(response);
+    // console.log(response);
     displayWeather(current, currentElement);
   });
 
