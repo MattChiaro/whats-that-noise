e91477dd5e991a62222d18f296fdd57253fea760
Matt Chiaro <131219830+MattChiaro@users.noreply.github.com>
2023-05-31 13:26:21 -0500

    refactor: housekeeping/template literals

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,21 @@
+//housekeeping
+
+const aTgFdFgW = "EBLlYPmsJS0INYOanmR3K7FG7BKYE1eg"; //ticketmaster key
+const RfGyArBH = 'f143fe1fd933ca340292950f394916e2'; //openweather key
+const city = "Chicago";
+const currentDay = dayjs();
+const endOfDay = currentDay.endOf("day").format();
+const currentDayAndTime = dayjs().format();
+
+
+
+
 const currentElement = document.getElementById("weatherinfo");
 
 // fetch geo coordinates
 
 fetch(
-  "http://api.openweathermap.org/geo/1.0/direct?q=chicago,IL,&limit=1&appid=f143fe1fd933ca340292950f394916e2"
+  `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${RfGyArBH}`
 )
   .then(function (response) {
     return response.json();
@@ -15,11 +27,7 @@ fetch(
     const state = lat_lon[0].state;
 
     var geo =
-      "https://api.openweathermap.org/data/2.5/weather?lat=" +
-      lat +
-      "&lon=" +
-      lon +
-      "&APPID=f143fe1fd933ca340292950f394916e2&units=imperial";
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${RfGyArBH}`;
 
     // fetch current weather condition , hard coded at the moment
     fetch(geo)
@@ -46,14 +54,10 @@ function displayWeather(weatherObject, weatherElement) {
   // weatherElement.innerHTML +="<br>";
   // weatherElement.innerHTML += `<img src="${iconUrl}">`;
 }
-const aTgFdFgW = "EBLlYPmsJS0INYOanmR3K7FG7BKYE1eg"; //key
-const city = "Chicago";
-const currentDay = dayjs();
-const endOfDay = currentDay.endOf("day").format();
-const currentDayAndTime = dayjs().format();
 
 //fetch ticketmaster API
 const ticketmasterApi = `https://app.ticketmaster.com/discovery/v2/events.json?city=${city}&segmentName=music&startDateTime=${currentDayAndTime}&endDateTime=${endOfDay}&size=40&sort=date,asc&apikey=${aTgFdFgW}`;
+
 fetch(ticketmasterApi)
   .then(function (response) {
     return response.json();
