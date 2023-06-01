7bc7048999167941c5d74cd8a41cd12a7f885e11
Matt Chiaro <131219830+MattChiaro@users.noreply.github.com>
2023-05-31 19:17:21 -0500

    refactor - limited nesting

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,13 +6,10 @@ const RfGyArBH = 'f143fe1fd933ca340292950f394916e2'; //openweather key
 const currentDay = dayjs();
 const endOfDay = currentDay.endOf("day").format();
 const currentDayAndTime = dayjs().format();
-const currentElement = document.getElementById("weatherinfo");
-let city = "";
+const currentWeatherEl = document.getElementById("weatherinfo");
 // fetch geo coordinates
 
-function searchCity() {
-    city = searchInput.value.trim()
-
+function fetchWeather(city) {
     const geoApi = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${RfGyArBH}`
 
     fetch(geoApi)
@@ -25,28 +22,29 @@ function searchCity() {
 
             const currentWeatherApi = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${RfGyArBH}`;
 
-            // fetch current weather condition , hard coded at the moment
+            // fetch current weather condition for city
             fetch(currentWeatherApi)
                 .then(function (response) {
                     return response.json();
                 })
                 .then(function (response) {
-                    const current = response;
-                    console.log(current);
-                    displayWeather(current, currentElement);
+                    const currentWeatherArray = response;
+                    displayWeather(currentWeatherArray, city);
                 });
         });
+}
 
-    function displayWeather(weatherObject, weatherElement) {
-        var iconUrl = `http://openweathermap.org/img/w/${weatherObject.weather[0].icon}.png`;
-        document.querySelector('#current-weather-title').textContent = `Current weather in ${city}:`;
-        weatherElement.innerHTML = `<p>${weatherObject.dt_txt || new Date().toLocaleDateString()}</p>
-  <p>Temp: ${weatherObject.main.temp} °F</p>
-  <p>Wind: ${weatherObject.wind.speed} MPH</p>
-  <p>Humidity: ${weatherObject.main.humidity}%</p>
-  <img src="${iconUrl}">`
-    }
+function displayWeather(currentWeatherArray, city) {
+    var iconUrl = `http://openweathermap.org/img/w/${currentWeatherArray.weather[0].icon}.png`;
+    document.querySelector('#current-weather-title').textContent = `Current weather in ${city}:`;
+    currentWeatherEl.innerHTML = `<p>${currentWeatherArray.dt_txt || new Date().toLocaleDateString()}</p>
+<p>Temp: ${currentWeatherArray.main.temp} °F</p>
+<p>Wind: ${currentWeatherArray.wind.speed} MPH</p>
+<p>Humidity: ${currentWeatherArray.main.humidity}%</p>
+<img src="${iconUrl}">`
+}
 
+function fetchTicketmaster(city) {
     //fetch ticketmaster API
     const ticketmasterApi = `https://app.ticketmaster.com/discovery/v2/events.json?city=${city}&segmentName=music&startDateTime=${currentDayAndTime}&endDateTime=${endOfDay}&size=40&sort=date,asc&apikey=${aTgFdFgW}`;
 
@@ -69,6 +67,16 @@ function searchCity() {
             }
         });
 }
+
+function searchCity() {
+
+    let city = searchInput.value.trim()
+
+    fetchWeather(city);
+    fetchTicketmaster(city);
+}
+
+
 
 document.querySelector("#search-button").addEventListener("click", searchCity);
 
