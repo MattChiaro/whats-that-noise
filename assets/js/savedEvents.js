8055a9cb93ceaa7b1a7754c40cd6c161480fa52d
Suraj <snair306@gmail.com>
2023-06-01 11:55:13 -0500

    commiting before pull

--- assets/js/savedEvents.js
+++ assets/js/savedEvents.js
@@ -1,3 +1,21 @@
 $(document).ready(function () {
     $('.collapsible').collapsible();
 });
+
+console.log(localStorage.getItem("event2" , "name"))
+const event2 = localStorage.getItem("event2");
+console.log(event2.name);
+
+const ulForSaved =  document.getElementById("savedeventsbox")
+console.log(localStorage.getItem("event2"));
+
+
+const liForSaved = document.createElement("li")
+ulForSaved.innerHTML = ""
+liForSaved.innerHTML = `
+<div class="collapsible-header"><i class="material-icons"</i></div>
+
+`
+ulForSaved.appendChild(liForSaved)
+
+//`<span class="title">${eventsArray[i].name}</span>
